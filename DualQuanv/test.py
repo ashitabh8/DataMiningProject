638c0f53e22e5739fb2113206eede49d6a1a093d
Utkarsh Sharma <usharma4@illinois.edu>
2024-05-06 20:27:35 -0500

    Update test.py

--- DualQuanv/test.py
+++ DualQuanv/test.py
@@ -2,49 +2,44 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset
-
-from model import QCNN_ReLU_BN 
+from model import QCNN_ReLU_BN
 from layers import DualConvNew, SwitchBN2d
 # from layers import DualQuanConv
 import numpy as np
-
-from utils import set_lower_upper_ratios_for_all_layers, train_model, test_model, normalize_dataset, set_bitwidths, get_random_configs
+from utils import set_lower_upper_ratios_for_all_layers, test_model, normalize_dataset, set_bitwidths, get_random_configs
 
 if __name__ == "__main__":
     data_dir = '/Users/utkarsh/cs512/data'
     test_file = data_dir + '/test_tensor_dataset_stft_3vehicles_pd_0_5_125_0_5.pth'
-
     test_dataset = torch.load(test_file)
-
     test_dataset = normalize_dataset(test_dataset)
-
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=True, num_workers=2)
 
-    bitwidth_non_pattern_bands = {2: 0.1, 4: 0.1, 8: 0.4, 16: 0.4} 
-    bitwidth_probs_pattern_band = {2: 0.5, 4: 0.3, 8: 0.1, 16: 0.1}
-    lower_bound_ratio = 0.70
-    upper_bound_ratio = 0.90
+    bitwidth_non_pattern_bands = {2: 0.4, 4: 0.3, 8: 0.2, 12: 0.1, 16: 0.0} 
+    bitwidth_probs_pattern_band = {2: 0.0, 4: 0.1, 8: 0.2, 12: 0.3, 16: 0.4}
 
-    # (pattern band bw, non-pattern band bw)
-    # bitwidth_allocation = [(4,8), (8,4), (16,8), (4,8), (8,8)]
-    file_name = '0.70_0.90_dual_quant_model_2_4_8_16.pth'
+    lower_bound_ratio = 0.33
+    upper_bound_ratio = 0.67
+
+    file_name = 'dual_quant_model_2_4_8_12_16.pth'
     running_model = QCNN_ReLU_BN(num_classes=3)
-
     print(f"Loading the best model")
     running_model.load_state_dict(torch.load(file_name))
-    
     criterion = nn.CrossEntropyLoss()
-    
-    # Generate random bitwidth configurations using get_random_configs
-    bitwidth_allocation = get_random_configs(running_model, bitwidth_probs_pattern_band, bitwidth_non_pattern_bands)
 
-    print(f"Generated bitwidth allocation: {bitwidth_allocation}")
+    num_iterations = 20  # Number of iterations to generate bitwidth allocations and test the model
+    test_accuracies = []
 
-    set_bitwidths(running_model, bitwidth_allocation)
-    set_lower_upper_ratios_for_all_layers(running_model, lower_bound_ratio, upper_bound_ratio)
+    for i in range(num_iterations):
+        # Generate random bitwidth configurations using get_random_configs
+        bitwidth_allocation = get_random_configs(running_model, bitwidth_probs_pattern_band, bitwidth_non_pattern_bands)
+        print(f"Generated bitwidth allocation: {bitwidth_allocation}")
 
-    test_model(running_model, test_loader, criterion)
+        set_bitwidths(running_model, bitwidth_allocation)
+        set_lower_upper_ratios_for_all_layers(running_model, lower_bound_ratio, upper_bound_ratio)
 
+        test_accuracy = test_model(running_model, test_loader, criterion)
+        test_accuracies.append(test_accuracy)
 
-
-
+    average_test_accuracy = sum(test_accuracies) / len(test_accuracies)
+    print(f"Average test accuracy: {average_test_accuracy:.4f}")
